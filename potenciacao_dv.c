4e7daef40c2f5d215ef9a0b700658206a18dd92f
XMATHEUSX <matheusxavier2302@gmail.com>
2022-04-16 12:05:41 -0300

    updating codes

--- potenciacao_dv.c
+++ potenciacao_dv.c
@@ -3,37 +3,37 @@
 #include <math.h>
 #include <stdlib.h>
 
-
-int potencia(a,n)
+int potencia(int a, int n)
 {
     int x;
     if (n == 0)
     {
         return 1;
     }
-    else if (n==1)
+    else if (n == 1)
     {
         return a;
     }
     else
     {
-        if (n%2 == 0)
-        { 
-            x = potencia(a,n/2);
-            return x*x;
+        if (n % 2 == 0)
+        {
+            x = potencia(a, n / 2);
+            return x * x;
         }
         else
         {
-            x = potencia(a,(n-1)/2);
-            return x*x*a;
+            x = potencia(a, (n - 1) / 2);
+            return x * x * a;
         }
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    int a,b,c;
-    scanf("%d %d",&a, &b);
-    c = potencia(a,b);
-    printf("%d",c);
+    int a, b, c;
+    scanf("%d %d", &a, &b);
+    c = potencia(a, b);
+    printf("%d", c);
+    return 0;
 }
